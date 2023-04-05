1aad3dc58e812ed1a19eaac7dede3513eac28c89
MartinL-no <martin.lorimer@icloud.com>
2023-04-05 14:18:45 +0200

    Add project files.

---
+++ OppgavePokemon/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OppgavePokemon
+{
+    /*
+        Task:
+        Create an interface called IPokemon that has a property Health, a method Attack()
+        and a method LooseHealth()
+        Create a class Magikarp that has a property IsUseless and a method called Splash.
+        Create another Class with your favorite pokemon, and methods named the same as the
+        attack types of the pokemon you have chosen. In the attack methods, you must enter
+        an IPokemon as a parameter and call it its LooseHealth method. 
+        Magikarp and your favorite pokemon class must both inherit from IPokemon and both
+        must have their own constructor that sets health. 
+        Create a few more more different Pokemen and put them all in a list 
+        List<IPokemon>wildPokemon in program.cs
+        Create a method that returns an IPokemon called GetWildPokemon() that selects and
+        returns a random pokemon from the wildPokemon list.
+        Define a while loop that retrieves a wild pokemon and lets it battle Magikarp until
+        Magikarp has 0 health
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
